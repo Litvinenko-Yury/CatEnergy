6141adaee455ce6fb8689c9a3b95af285c8aa68f
Литвиненко Юрий <litvinenko-don@ukr.net>
2019-10-12 14:06:37 +0300

    Content overflow test ok

--- source/js/script.js
+++ source/js/script.js
@@ -2,12 +2,6 @@
 
 var burger = document.querySelector(".burger");
 var mainNav = document.querySelector(".main-nav__list");
-/*===*/
-// var btnBeforeMob = document.querySelector(".field-range-mob__btn-before");
-// var btnAfterMob = document.querySelector(".field-range-mob__btn-after");
-// var imgSubContainer1 = document.querySelector(".compare__img-subcontainer-1");
-// var imgSubContainer2 = document.querySelector(".compare__img-subcontainer-2");
-// var runnable = document.querySelector(".field-range-mob__wrap");
 
 /*=====*/
 /*если JS есть, для mob свернуть меню и показать бургер*/
